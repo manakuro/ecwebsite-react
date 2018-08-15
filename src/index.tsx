9a9f8c3b4be6f831f7179bd2a0118958ca69e1c7
manato kuroda <a7108100@gmail.com>
2018-08-15 09:27:25 +0900

    refactor: refactor some code

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+// global css
 import 'sanitize.css'
 import './index.css'
 import 'font-awesome/css/font-awesome.min.css'
